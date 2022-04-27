c15201eecbb166ad5a63979a2c7ddc4693beaaf6
Atul <atul.intern@phonepe.com>
2022-04-27 16:37:24 +0530

    Tested ConfigTree comparison  with several use cases but have to implement it now

--- test.py
+++ test.py
@@ -19,15 +19,30 @@ server {
       namespace Tikku {}
       namespace Atul {
         thik mem
-        bellar { name tikku }
+        juli aunty
+        bellar { name atul }
         }
       namespace pinki {}
 }
 '''
-obj=ConfigTree()
-ConfigTree.process(text2,0,obj)
+obj1=ConfigTree()
+ConfigTree.process(text1,0,obj1)
 #print(obj.children[0].children[0].children[1].data)
-data=ConfigTree.genPaths(obj.children[0])
-for i in data:
-  print(i,end='\n\n')
+data=ConfigTree.genPaths(obj1.children[0])
+ConfigTree.paths=[]
+
+obj2=ConfigTree()
+ConfigTree.process(text2,0,obj2)
+#print(obj.children[0].children[0].children[1].data)
+data2=ConfigTree.genPaths(obj2.children[0])
+
+
+count=0
+for path1 in data:
+  for path2 in data2:
+    if path1==path2:
+      print(path1,'=',path2)
+      count+=1
+print(count)
+
 
