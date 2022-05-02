aa089e390ef18c4114e72b081bf5aa53b563813c
Atul <atul.intern@phonepe.com>
2022-05-02 13:04:07 +0530

    Saving for security of code

--- init.py
+++ init.py
@@ -7,15 +7,15 @@ import requests
 from hostfinder import HostsFinder
 from ConfigTree import ConfigTree
 
-tasks=[1,2,3]
+
 app_start_command="python3 flask.py &"
 conf_location="/etc/aerospike/aerospike.conf"
 
 def startMaster():
     os.system(app_start_command)
 
-def doClientWork():
-    r=requests.get("http://",ad,"/conf")
+def doClientWork(maddr):
+    r=requests.get("http://",maddr,"/conf")
     mconf=r.text()
     sconf=open(conf_location,"r").read()
     isequal,mtree,stree=ConfigTree.isSame(mconf,sconf)
@@ -24,23 +24,24 @@ def doClientWork():
     msconfig=ConfigTree.makeCorrectConfig(mtree,stree)
     open(conf_location,"w").write(msconfig)
 
+def doInterNodeTask():
+    pass
 
-
-if 1 in tasks:
-    addrs=HostsFinder.getAddresses()
-    addrs.sort()
-    for ad in addrs:
-        if os.system("ping -c 1 ",ad)==0:
-            #ad is master
-            if os.uname().hostname in ad:
-                #this is ad and master
-                if requests.get("http://",ad,"/conf").status_code==200:
-                    pass
-                else:
-                    startMaster()
+addrs=HostsFinder.getAddresses()
+addrs.sort()
+for ad in addrs:
+    if os.system("ping -c 1 ",ad)==0:
+        #ad is master
+        if os.uname().hostname in ad:
+            #this is ad and master
+            if requests.get("http://",ad,"/conf").status_code==200:
+                pass
             else:
-                doClientWork(ad)
-             
+                startMaster()
+        else:
+            doClientWork(ad)
+
+doInterNodeTask()             
 
 
 
