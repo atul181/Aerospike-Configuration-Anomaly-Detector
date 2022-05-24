35b93add166907c49ca086c098c0bca9a353febf
atul.intern <atul.intern@phonepe.com>
2022-05-24 12:41:06 +0530

    Saving..

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-cp init.py ConfigTree.py flaskserver.py hostfinder.py ptoken testaerospike.service /Users/atul.intern/Deploy/
-cp init.py ConfigTree.py flaskserver.py hostfinder.py ptoken testaerospike.service ../aspy/
+cp init.py ConfigTree.py flaskserver.py hostfinder.py  testaerospike.service /Users/atul.intern/Deploy/
+cp init.py ConfigTree.py flaskserver.py hostfinder.py  testaerospike.service ../aspy/
 scp /Users/atul.intern/Deploy/* sre@stg-atul001.phonepe.nb6:/home/sre/testaerospike/
 scp /Users/atul.intern/Deploy/* sre@stg-atul002.phonepe.nb6:/home/sre/testaerospike/
 scp /Users/atul.intern/Deploy/* sre@stg-atul003.phonepe.nb6:/home/sre/testaerospike/
