a304404c38628917205a03ea306b035f0f06c1ed
atul.intern <atul.intern@phonepe.com>
2022-06-14 15:52:44 +0530

    Removed unused files

--- restartService.sh
+++
@@ -1,6 +0,0 @@
-systemctl stop testaerospike.service
-systemctl disable testaerospike.service
-systemctl daemon-reload
-systemctl enable testaerospike.service
-systemctl start testaerospike.service
-systemctl status testaerospike.service
